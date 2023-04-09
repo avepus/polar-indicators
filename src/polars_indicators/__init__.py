29d2115a4d41b00307d38208c72abbde94bdba66
avepus <avepus@gmail.com>
2023-04-09 15:15:42 -0500

    limiting success

--- src/polars_indicators/__init__.py
+++ src/polars_indicators/__init__.py
@@ -154,38 +154,44 @@ def targeted_value(df: pl.DataFrame | pl.LazyFrame, targets: str) -> IndicatorRe
     return IndicatorResult(df, column_name)
 
 
-def group_by_bars(df: pl.DataFrame | pl.LazyFrame, nulls: int, column: str) -> IndicatorResult:
-    """groups trades together allowing for a specified number of nulls in each group"""
-    column_name = f"group_{nulls}"
-    if column_name in df.columns:
-        return IndicatorResult(df, column_name)
-    
-    tes = "tes"
-    df = df.with_columns(pl.lit(True).alias(tes))
-    
-    for i in range(1, nulls+1):
-        df = df.with_columns(pl.col(tes) & pl.col(column).shift(i).is_null().alias(tes))
-
-    df = df.with_columns(pl.when(pl.col(tes)).then(1).otherwise(0).cumsum().alias(column_name)) 
-
-    return IndicatorResult(df, column_name)
 def limit_entries(df: pl.DataFrame | pl.LazyFrame, bars: int, entries: str) -> IndicatorResult:
     """Forces a minimum number of bars between entries"""
+    def group_by_bars(df: pl.DataFrame | pl.LazyFrame, nulls: int, column: str) -> IndicatorResult:
+        """assigns an ID to each group of consecutive non-null values allowing for an
+        input (nulls) number of nulls before considering a set a new group"""
+        column_name = f"group_{nulls}"
+        if column_name in df.columns:
+            return IndicatorResult(df, column_name)
+        
+        tes = "tes"
+        df = df.with_columns(pl.lit(True).alias(tes))
+        for i in range(1, nulls+1):
+            df = df.with_columns(pl.col(tes) & pl.col(column).shift(i).is_null().alias(tes))
+        df = df.with_columns(pl.when(pl.col(tes)).then(1).otherwise(0).cumsum().alias(column_name)) 
+        return IndicatorResult(df, column_name)
+    
     column_name = f"{bars}_minimum_bars_between"
     if column_name in df.columns:
         return IndicatorResult(df, column_name)
     
-    group = group_by_bars(df, bars, entries)
+    new_columns = df.columns.copy()
+    new_columns.append(column_name)
+
+    lf = df.lazy()
+    
+    group = group_by_bars(lf, bars, entries)
 
     ones = "ones"
 
-    df = group.df
+    lf = group.df
 
     sum = "sum"
-    df = group.df.with_columns(pl.lit(1).alias(ones)).with_columns(pl.col(ones).cumsum().over(group.column).alias(sum))
+    lf = group.df.with_columns(pl.lit(1).alias(ones)).with_columns(pl.col(ones).cumsum().over(group.column).alias(sum))
 
-    df = df.with_columns(pl.when(pl.col(sum) % (bars+1) == 1).then(pl.col(entries)).alias(column_name))
+    lf = lf.with_columns(pl.when(pl.col(sum) % (bars+1) == 1).then(pl.col(entries)).alias(column_name))
 
+    lf = lf.select(new_columns)
+    df = lf if isinstance(df, pl.LazyFrame) else lf.collect()
 
     return IndicatorResult(df, column_name)
 
@@ -197,6 +203,7 @@ def create_trade_ids_old(df: pl.DataFrame | pl.LazyFrame, enter_column: str, exi
     column_name = f"{enter_column}/{exit_column}"
     if column_name in df.columns:
         return IndicatorResult(df, column_name)
+        
     
     exit_ids = 'exit_ids'
     
