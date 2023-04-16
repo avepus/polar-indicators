0caf255e03c0a2adc63cf468bad3884a75f48b42
avepus <avepus@gmail.com>
2023-04-16 09:55:16 -0500

    cleaned up magic numbers to pramaters

--- src/polars_indicators/strategies/buy_x_week_low.py
+++ src/polars_indicators/strategies/buy_x_week_low.py
@@ -4,28 +4,41 @@ import polars_indicators as pi
 from polars_indicators.strategies.strategy_result import StrategyResult
 
 
-def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta) -> StrategyResult:
-    """generates trades on input df
-    this will filter out all data from before we have a full min from the lookback period
-    see polars 'rolling_' documentation for all options"""
+def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta, offset_percentage: int, entry_stop_percentage: int, trailing_stop_bars: int) -> StrategyResult:
+    """Generates trades on input df
+
+    This will filter out all data from before we have a full min from the lookback period
+    see polars 'rolling_' documentation for all options
+
+    Args:
+        df: DataFrame instance with OHLCV columns
+        lookback: how long the rolling min is
+        offset_percentage: how much to offset the limit order from the min
+        entry_stop_percentage: how much to offset the entry bar stop-loss value from
+          the entry limit order price
+        trailing_stop_bars: number of bars trailing stop considers
+
+    Returns:
+        A StategyResult representing the result of running the strategy
+        on the input data
+    """
+    factor = (100 + offset_percentage) / 100
 
     weeks_min = f"{lookback}_week_min"
-    df = df.with_columns(pl.col("Low").rolling_min(lookback, by=pi.indicators.DATE_COLUMN).over(pi.indicators.SYMBOL_COLUMN).alias(weeks_min))
+    df = df.with_columns((pl.col("Low") * factor).rolling_min(
+        lookback, by=pi.indicators.DATE_COLUMN).over(
+            pi.indicators.SYMBOL_COLUMN).alias(weeks_min))
     filter_datetime = df[pi.indicators.DATE_COLUMN].min() + lookback
     df = df.filter(pl.col(pi.indicators.DATE_COLUMN) > filter_datetime) #this filters out data that doesn't have the full lookback
-
-    #magic number should be parameter. filters to make sure we dont get small stuff
-    df = df.filter(pl.col(pi.indicators.CLOSE_COLUMN).mean().over(pi.indicators.SYMBOL_COLUMN) > 10) 
 
     target = pi.indicators.targeted_value(df, weeks_min)
     enter_column = target.column
     df = target.df
     
-    percentage = -5
-    percentage_stop = pi.indicators.entry_percentage_stop(df, percentage, target.column)
+    percentage_stop = pi.indicators.entry_percentage_stop(df, entry_stop_percentage, target.column)
     df = percentage_stop.df
 
-    trail = pi.indicators.trailing_stop(df, bars=2) #magic number should be parameter
+    trail = pi.indicators.trailing_stop(df, bars=trailing_stop_bars)
 
     eod = pi.indicators.end_of_data_stop(trail.df)
     df = eod.df
