d071e46f4c98abd72c1970cf5b949558c66e23ad
avepus <avepus@gmail.com>
2023-04-16 21:25:51 -0500

    updates to buy_x_week_low

--- src/polars_indicators/strategies/buy_x_week_low.py
+++ src/polars_indicators/strategies/buy_x_week_low.py
@@ -4,7 +4,7 @@ import polars_indicators as pi
 from polars_indicators.strategies.strategy_result import StrategyResult
 
 
-def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta, offset_percentage: int, entry_stop_percentage: int, trailing_stop_bars: int) -> StrategyResult:
+def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta, offset_percentage: int, entry_stop_percentage: int, trailing_stop_bars: int, min_price: float) -> StrategyResult:
     """Generates trades on input df
 
     This will filter out all data from before we have a full min from the lookback period
@@ -17,6 +17,7 @@ def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta, offset_percen
         entry_stop_percentage: how much to offset the entry bar stop-loss value from
           the entry limit order price
         trailing_stop_bars: number of bars trailing stop considers
+        min_price: minimum price required to attempt a trade on a bar
 
     Returns:
         A StategyResult representing the result of running the strategy
@@ -24,13 +25,21 @@ def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta, offset_percen
     """
     factor = (100 + offset_percentage) / 100
 
+    df = df.lazy()
+
     weeks_min = f"{lookback}_week_min"
     df = df.with_columns((pl.col("Low") * factor).rolling_min(
         lookback, by=pi.indicators.DATE_COLUMN).over(
             pi.indicators.SYMBOL_COLUMN).alias(weeks_min))
     
+    #min_price filter
+    df = df.with_columns(pl.when(pl.col(weeks_min) >= min_price).then(pl.col(weeks_min)))
+    
+    #filter data that doesn't have the full lookback
+    df = df.collect() #we can't subscript a lazyframe on the line below so we collect first here
     filter_datetime = df[pi.indicators.DATE_COLUMN].min() + lookback
-    df = df.filter(pl.col(pi.indicators.DATE_COLUMN) > filter_datetime) #this filters out data that doesn't have the full lookback
+    df = df.lazy() 
+    df = df.filter(pl.col(pi.indicators.DATE_COLUMN) > filter_datetime)
 
     target = pi.indicators.targeted_value(df, weeks_min)
     enter_column = target.column
@@ -52,7 +61,7 @@ def strategy(df: pl.DataFrame | pl.LazyFrame, lookback: timedelta, offset_percen
 
     ids = pi.indicators.create_trade_ids(df, enter_column, exit_column)
 
-    df = ids.df
+    df = ids.df.collect()
 
     return StrategyResult(df, enter_column, exit_column, ids.column )
     
