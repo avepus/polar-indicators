cc52eeef93ef1fb10a47aa930b7762b2fa0a7e34
avepus <avepus@gmail.com>
2023-05-02 22:28:30 -0500

    testing

--- src/polars_indicators/strategies/buy_x_week_low.py
+++ src/polars_indicators/strategies/buy_x_week_low.py
@@ -3,7 +3,7 @@ import polars_indicators as pi
 from polars_indicators.strategies.strategy_result import StrategyResult
 
 
-def strategy(df: pl.DataFrame | pl.LazyFrame, lookback_bars: int, offset_bars: int, offset_percentage: int, entry_stop_percentage: int, trailing_stop_bars: int, min_price: float) -> StrategyResult:
+def strategy(df: pl.DataFrame | pl.LazyFrame, lookback_bars: int, offset_bars: float, offset_percentage: int, entry_stop_percentage: int, trailing_stop_bars: int, min_price: float, additional_criteria: str=None) -> StrategyResult:
     """Generates trades on input df
 
     This will filter out all data from before we have a full min from the lookback period
@@ -18,6 +18,7 @@ def strategy(df: pl.DataFrame | pl.LazyFrame, lookback_bars: int, offset_bars: i
           the entry limit order price
         trailing_stop_bars: number of bars trailing stop considers
         min_price: minimum price required to attempt a trade on a bar
+        additional_criteria: a bool series that must be true in order to attempt a trade
 
     Returns:
         A StategyResult representing the result of running the strategy
@@ -43,6 +44,10 @@ def strategy(df: pl.DataFrame | pl.LazyFrame, lookback_bars: int, offset_bars: i
     target = pi.indicators.targeted_value(df, roll_min.column)
     enter_column = target.column
     df = target.df
+
+    #take additional criteria into account
+    if additional_criteria:
+        df = df.with_columns(pl.when(pl.col(additional_criteria)).then(pl.col(enter_column)))
     
     percentage_stop = pi.indicators.entry_percentage_stop(df, entry_stop_percentage, target.column)
     df = percentage_stop.df
@@ -64,5 +69,28 @@ def strategy(df: pl.DataFrame | pl.LazyFrame, lookback_bars: int, offset_bars: i
 
 
     return StrategyResult(df, enter_column, exit_column, ids.column)
+
+
+def fluctuation_and_relative_volume(df: pl.DataFrame | pl.LazyFrame, fluctuation_sma_length: int, relative_volume_sma_length: int, lookback_bars: int=7, offset_bars: int=3, offset_percentage: float=1, entry_stop_percentage: int=-5, trailing_stop_bars: int=2, min_price: float=3) -> StrategyResult:
+    """uses fluctuation and relative volume to determine when to trade"""
+    fluctuation = pi.indicators.fluctuation_percentage(df)
+    fluct_sma3 = pi.indicators.simple_moving_average(fluctuation.df, fluctuation_sma_length, fluctuation.column)
+    grouped_fluctation = pi.indicators.group_by_amount(fluct_sma3.df, fluct_sma3.column, [1,2,3,5,7,10,20])
+    df = grouped_fluctation.df
+
+    relative_volume = pi.indicators.relative_volume(df, relative_volume_sma_length)
+    grouped_relative_volume = pi.indicators.group_by_amount(relative_volume.df, relative_volume.column, [x * 25 for x in range(1,7)])
+    df = grouped_relative_volume.df
+
+    additional_criteria = "additional_criteria"
+
+    df = df.with_columns(
+        (
+            ((pl.col(grouped_fluctation.column) >= 10) & (pl.col(grouped_relative_volume.column) == 25))
+            |
+            ((pl.col(grouped_fluctation.column) == 20.01) & (pl.col(grouped_relative_volume.column) == 50))
+        ).alias(additional_criteria))
+    
+    return strategy(df, lookback_bars, offset_bars, offset_percentage, entry_stop_percentage, trailing_stop_bars, min_price, additional_criteria)
     
         
