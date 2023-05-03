f6221f185b2c4aac9fba3b18aa90f2f2802ae73a
avepus <avepus@gmail.com>
2023-05-02 22:16:25 -0500

    removed unused code

--- src/polars_indicators/summarize/trades.py
+++ src/polars_indicators/summarize/trades.py
@@ -77,39 +77,3 @@ class Trades:
         return self.df.select(columns
             
             )
-
-
-def zzsummarize_trades(df: pl.DataFrame | pl.LazyFrame, trade_id_column: str, enter_column: str, exit_column: str, additional_columns: list[str]) -> pl.DataFrame | pl.LazyFrame:
-    """summarizes trade information given ids in input column
-    allows for keeping additional column values as they were at the start of the trade
-    PROTOTYPE. NEEDS MORE WORK AND MAY NOT BE THE DIRECTION I GO"""
-    index_name = "index"
-    start = "Start"
-    end = "End"
-    entry_price = "Entry_Price"
-    exit_price = "Exit_Price"
-    length = "Length"
-    df = df.drop_nulls(trade_id_column).with_row_count(index_name).groupby(trade_id_column).agg(
-              [pl.when(pl.col(pi.indicators.DATE_COLUMN) == pl.col(pi.indicators.DATE_COLUMN).max()).then(pl.col(col)).min().alias(col) for col in additional_columns],
-              pl.col(pi.indicators.SYMBOL_COLUMN).min().alias(pi.indicators.SYMBOL_COLUMN),
-              pl.col(pi.indicators.DATE_COLUMN).min().alias(start),
-              pl.col(pi.indicators.DATE_COLUMN).max().alias(end),
-              pl.when(pl.col(pi.indicators.DATE_COLUMN) == pl.col(pi.indicators.DATE_COLUMN).min()).then(pl.col(enter_column)).min().alias(entry_price),
-              pl.when(pl.col(pi.indicators.DATE_COLUMN) == pl.col(pi.indicators.DATE_COLUMN).max()).then(pl.col(exit_column)).min().alias(exit_price),
-              pl.col(pi.indicators.LOW_COLUMN).min().alias(pi.indicators.LOW_COLUMN),
-              pl.col(pi.indicators.HIGH_COLUMN).max().alias(pi.indicators.HIGH_COLUMN),
-              (pl.col(index_name).max() - pl.col(index_name).min() + 1).alias(length)
-    ).sort(trade_id_column)
-
-    net_gain = "Gain/Loss"
-    net_gain_percent = "Gain/Loss%"
-    highest_percent = "Highest%"
-    lowest_percent = "Lowest%"
-    df = df.with_columns(
-        (pl.col(exit_price) - pl.col(entry_price)).alias(net_gain),
-        ((pl.col(exit_price) - pl.col(entry_price)) / pl.col(entry_price) * 100).round(2).alias(net_gain_percent),
-        ((pl.col(HIGH_COLUMN) - pl.col(entry_price)) / pl.col(entry_price) * 100).round(2).alias(highest_percent),
-        ((pl.col(LOW_COLUMN) - pl.col(entry_price)) / pl.col(entry_price) * 100).round(2).alias(lowest_percent),
-        )
-
-    return df
